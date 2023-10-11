639c7ee66df8c1c97a5cd136487ae00947f5ceeb
Maddie Portrey <maddiekay13@gmail.com>
2023-10-11 13:42:00 -0600

    penalized regression code

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -12,25 +12,55 @@ amazonTrain$ACTION <- as.factor(amazonTrain$ACTION)
 
 my_recipe <- recipe(ACTION~., data = amazonTrain) %>%
   step_mutate_at(all_numeric_predictors(), fn = factor) %>%
-  step_other(all_factor_predictors(), threshold = .01) %>%
-  step_dummy(all_nominal_predictors()) %>%
-  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
-  step_mutate_at(all_numeric_predictors(), fn = factor)
-  
-#prep <- prep(my_recipe)
-#baked <- bake(prep, new_data = amazonTrain)
+  step_other(all_nominal_predictors(), threshold = .001) %>%
+  #step_dummy(all_nominal_predictors()) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) #%>%
+  #step_mutate_at(all_numeric_predictors(), fn = factor)
 
-my_mod <- logistic_reg() %>%
-  set_engine("glm")
+# my_mod <- logistic_reg() %>%
+#   set_engine("glm")
+# 
+# amazon_workflow <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(my_mod) %>%
+#   fit(data = amazonTrain)
+# 
+# amazon_preds <- predict(amazon_workflow, 
+#                         new_data = amazonTest,
+#                         type="prob")
+# 
+# preds <- cbind(amazonTest$id, amazon_preds$.pred_1)
+# colnames(preds) <- c("Id","ACTION")
+# preds <- as.data.frame(preds)
+# vroom_write(preds, "amazon_predictions.csv", ",")
+
+my_mod <- logistic_reg(mixture = tune(),
+                       penalty = tune()) %>%
+  set_engine("glmnet")
 
 amazon_workflow <- workflow() %>%
   add_recipe(my_recipe) %>%
-  add_model(my_mod) %>%
-  fit(data = amazonTrain)
+  add_model(my_mod)
 
-amazon_preds <- predict(amazon_workflow, 
-                        new_data = amazonTest,
-                        type="prob")
+tuning_grid <- grid_regular(penalty(),
+                            mixture(),
+                            levels = 5)
+
+folds <- vfold_cv(amazonTrain, v = 10, repeats = 1)
+
+CV_results <- amazon_workflow %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(roc_auc))
+
+bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+final_wf <- amazon_workflow %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = amazonTrain)
+bestTune
+amazon_preds <- final_wf %>% predict(new_data = amazonTest, type = "prob")
 
 preds <- cbind(amazonTest$id, amazon_preds$.pred_1)
 colnames(preds) <- c("Id","ACTION")
