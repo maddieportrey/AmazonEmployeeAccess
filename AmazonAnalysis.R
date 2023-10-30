3ffd3937fffca68875a003e2fdd3da1f0eae8689
Maddie Portrey <maddiekay13@gmail.com>
2023-10-30 14:44:13 -0600

    update code

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -1,11 +1,94 @@
+# library(tidyverse)
+# library(vroom)
+# library(tidymodels)
+# library(embed)
+# library(ranger)
+# library(doParallel)
+# 
+# #112 columns
+# 
+# amazonTrain <- vroom("./train.csv")
+# amazonTest <- vroom("./test.csv")
+# amazonTrain$ACTION <- as.factor(amazonTrain$ACTION)
+# 
+# my_recipe <- recipe(ACTION~., data = amazonTrain) %>%
+#   step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+#   step_other(all_nominal_predictors(), threshold = .001) %>%
+#   #step_dummy(all_nominal_predictors()) %>%
+#   step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) #%>%
+#   #step_mutate_at(all_numeric_predictors(), fn = factor)
+# 
+# # my_mod <- logistic_reg() %>%
+# #   set_engine("glm")
+# # 
+# # amazon_workflow <- workflow() %>%
+# #   add_recipe(my_recipe) %>%
+# #   add_model(my_mod) %>%
+# #   fit(data = amazonTrain)
+# # 
+# # amazon_preds <- predict(amazon_workflow, 
+# #                         new_data = amazonTest,
+# #                         type="prob")
+# # 
+# # preds <- cbind(amazonTest$id, amazon_preds$.pred_1)
+# # colnames(preds) <- c("Id","ACTION")
+# # preds <- as.data.frame(preds)
+# # vroom_write(preds, "amazon_predictions.csv", ",")
+# 
+# # my_mod <- logistic_reg(mixture = tune(),
+# #                        penalty = tune()) %>%
+# #   set_engine("glmnet")
+# 
+# my_mod <- rand_forest(mtry = tune(),
+#                       min_n = tune(),
+#                       trees = 500) %>%
+#   set_engine("ranger") %>%
+#   set_mode("classification")
+# 
+# amazon_workflow <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(my_mod)
+# 
+# # tuning_grid <- grid_regular(penalty(),
+# #                             mixture(),
+# #                             levels = 5)
+# 
+# tuning_grid <- grid_regular(mtry(range = c(1,9)),
+#                             min_n(),
+#                             levels = 5)
+# 
+# folds <- vfold_cv(amazonTrain, v = 10, repeats = 1)
+# cl <- makePSOCKcluster(10)
+# registerDoParallel(cl)
+# CV_results <- amazon_workflow %>%
+#   tune_grid(resamples = folds,
+#             grid = tuning_grid,
+#             metrics = metric_set(roc_auc))
+# stopCluster(cl)
+# bestTune <- CV_results %>%
+#   select_best("roc_auc")
+# 
+# final_wf <- amazon_workflow %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data = amazonTrain)
+# bestTune
+# amazon_preds <- final_wf %>% predict(new_data = amazonTest, type = "prob")
+# 
+# preds <- cbind(amazonTest$id, amazon_preds$.pred_1)
+# colnames(preds) <- c("Id","ACTION")
+# preds <- as.data.frame(preds)
+# vroom_write(preds, "amazon_predictions.csv", ",")
+
+#random forest on smote balanced data
+
+
 library(tidyverse)
 library(vroom)
 library(tidymodels)
 library(embed)
 library(ranger)
 library(doParallel)
-
-#112 columns
+library(themis)
 
 amazonTrain <- vroom("./train.csv")
 amazonTest <- vroom("./test.csv")
@@ -13,31 +96,11 @@ amazonTrain$ACTION <- as.factor(amazonTrain$ACTION)
 
 my_recipe <- recipe(ACTION~., data = amazonTrain) %>%
   step_mutate_at(all_numeric_predictors(), fn = factor) %>%
-  step_other(all_nominal_predictors(), threshold = .001) %>%
-  #step_dummy(all_nominal_predictors()) %>%
-  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) #%>%
-  #step_mutate_at(all_numeric_predictors(), fn = factor)
-
-# my_mod <- logistic_reg() %>%
-#   set_engine("glm")
-# 
-# amazon_workflow <- workflow() %>%
-#   add_recipe(my_recipe) %>%
-#   add_model(my_mod) %>%
-#   fit(data = amazonTrain)
-# 
-# amazon_preds <- predict(amazon_workflow, 
-#                         new_data = amazonTest,
-#                         type="prob")
-# 
-# preds <- cbind(amazonTest$id, amazon_preds$.pred_1)
-# colnames(preds) <- c("Id","ACTION")
-# preds <- as.data.frame(preds)
-# vroom_write(preds, "amazon_predictions.csv", ",")
-
-# my_mod <- logistic_reg(mixture = tune(),
-#                        penalty = tune()) %>%
-#   set_engine("glmnet")
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize(all_predictors()) %>%
+  step_pca(all_predictors(), threshold=.8) %>%
+  step_smote(all_outcomes(), neighbors = 5) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION))
 
 my_mod <- rand_forest(mtry = tune(),
                       min_n = tune(),
@@ -48,10 +111,6 @@ my_mod <- rand_forest(mtry = tune(),
 amazon_workflow <- workflow() %>%
   add_recipe(my_recipe) %>%
   add_model(my_mod)
-
-# tuning_grid <- grid_regular(penalty(),
-#                             mixture(),
-#                             levels = 5)
 
 tuning_grid <- grid_regular(mtry(range = c(1,9)),
                             min_n(),
@@ -67,11 +126,11 @@ CV_results <- amazon_workflow %>%
 stopCluster(cl)
 bestTune <- CV_results %>%
   select_best("roc_auc")
-
+show_notes(.Last.tune.result)
 final_wf <- amazon_workflow %>%
   finalize_workflow(bestTune) %>%
   fit(data = amazonTrain)
-bestTune
+
 amazon_preds <- final_wf %>% predict(new_data = amazonTest, type = "prob")
 
 preds <- cbind(amazonTest$id, amazon_preds$.pred_1)
